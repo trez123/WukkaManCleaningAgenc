4472bde67def63a103ccaae30eb6d1fd32c76f7e
Silas Linux <silascoley3@gmail.com>
2023-11-17 01:22:36 -0500

    employee controller added

--- WukkamanCleaningAgencyApi/Data/WukkamanDbContext.cs
+++ WukkamanCleaningAgencyApi/Data/WukkamanDbContext.cs
@@ -4,11 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WukkamanCleaningAgencyApi.Data
 {
-    public class WukkamanDbContext : IdentityDbContext
+    public class WukkamanDbContext : DbContext
     {
         public WukkamanDbContext(DbContextOptions<WukkamanDbContext> options) : base(options) { }
 
-        public DbSet<Employee> Students { get; set; }
+        public DbSet<Employee> Employees { get; set; }
 
         public DbSet<Shift> Shifts { get; set; }
     }
